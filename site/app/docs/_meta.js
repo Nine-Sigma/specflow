ca80d3699aca55c38972b3c1ab699cb53cd558db
deanban <3989225+deanban@users.noreply.github.com>
2026-02-05 00:27:14 -0500

    feat(39-03): transform tech writer reports to MDX site content

---
+++ site/app/docs/_meta.js
@@ -0,0 +1,8 @@
+export default {
+  index: 'Introduction',
+  'getting-started': 'Getting Started',
+  commands: 'Commands',
+  architecture: 'Architecture',
+  methodology: 'Methodology',
+  agents: 'Agents',
+}
